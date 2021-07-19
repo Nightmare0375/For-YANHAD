ce4c3b106cd83fe07efe112e05c3dbc124a40c92
Muhammad Altaee <85696834+Nightmare0375@users.noreply.github.com>
2021-07-19 06:37:19 +0300

    Add files via upload

---
+++ Yn.js
@@ -0,0 +1,8 @@
+function myFunction () {
+	document.getElementById('content-2').style.display = 'inline-block';
+	document.getElementById('content-1').style.display = 'none';
+}
+function secF() {
+	document.getElementById('content-1').style.display = 'inline-block';
+	document.getElementById('content-2').style.display = 'none';
+}
